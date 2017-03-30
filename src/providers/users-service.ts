360691d7519004ecb02bab8eb9ef09b9e82842f2
natansevero <natanseverolp@gmail.com>
2017-03-30 11:54:32 -0300

    Login finalizado

--- src/providers/users-service.ts
+++ src/providers/users-service.ts
@@ -5,8 +5,8 @@ import 'rxjs/add/operator/toPromise';
 @Injectable()
 export class UsersService {
 
-  // private apiUri = 'https://share-photo-api-nodejs.herokuapp.com';
-  private apiUri = 'http://localhost:3000';
+  private apiUri = 'https://share-photo-api-nodejs.herokuapp.com';
+  // private apiUri = 'http://localhost:3000';
   private headers = new Headers({"Content-Type": "application/json"});
 
   constructor(public http: Http) {
